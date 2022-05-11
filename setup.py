1001f4368cc56cd781090ccbb9f6886c5595e817
root <jiachen.xu.94@gmail.com>
2022-05-11 20:02:22 +0200

    update setup file

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/TateXu/pytes",
     packages=['pytes'],
-    package_dir={'pytes': 'pytes/pytes'},
+    package_dir={'pytes': 'pytes/'},
     package_data={'pytes': ['example_data/*.pkl']},
     classifiers=[
         "Programming Language :: Python :: 3.7",
