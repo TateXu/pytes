5af7c973603353eef6071cee64ef92d1d6d8d180
Jiachen Xu <jiachen.xu@univie.ac.at>
2022-05-11 15:11:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["python>=3"]
+requirements = ["python>=3.6"]
 
 setup(
     name="PyTES",
