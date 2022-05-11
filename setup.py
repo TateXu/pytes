d8c68aec3fa552abaae5fffda6541862ca61acd4
Jiachen Xu <jiachen.xu@univie.ac.at>
2022-05-11 15:12:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/TateXu/pytes",
     packages=find_packages(),
-    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: 3-clause BSD",
